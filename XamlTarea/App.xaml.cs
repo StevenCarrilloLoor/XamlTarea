6417de734dc64c588d07c0c1739d2050beb536d8
StevenCarrilloLoor <stevencarrilloloor@gmail.com>
2024-05-23 23:08:41 -0500

    Agregar archivos de proyecto.

---
+++ XamlTarea/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
+using Application = Microsoft.Maui.Controls.Application;
+
+namespace XamlTarea
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CustomLayoutPage();
+        }
+    }
+}
---
+++ XamlTarea/CustomLayoutPage.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace XamlTarea
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomLayoutPage : ContentPage
+    {
+        public CustomLayoutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
